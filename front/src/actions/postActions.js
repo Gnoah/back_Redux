49e90f8981e884c6911b49a3ee913f29dc287f30
Gnoah <root@localhost.localdomain>
2019-05-13 13:35:38 +0300

    vita

---
+++ front/src/actions/postActions.js
@@ -0,0 +1,40 @@
+import { FETCH_POSTS, NEW_POST } from './types.js';
+import axios from 'axios';
+//import 'whatwg-fetch'
+
+export const fetchPosts = () => dispatch => {
+  console.log('fetching');
+
+  fetch('http://localhost:8070/eleve')
+    .then(res => res.json())
+    .then(posts =>
+      dispatch({
+        type: FETCH_POSTS,
+        payload: posts
+      })
+    );
+};
+
+export const createPost = postData => dispatch => {
+    console.log('action called',postData);
+    axios({
+        method: 'post',
+        url: 'http://localhost:8070/eleve',
+        data: postData
+      })
+  
+    // fetch('https://localhost:8070/eleve', {
+    //   method: 'POST',
+    //   headers: {
+    //     'content-type': 'application/json'
+    //   },
+    //   body: JSON.stringify(postData)
+    //  })
+      .then(res => res.json())
+      .then(post =>
+        dispatch({
+          type: NEW_POST,
+          payload: post
+        })
+      );
+  };
